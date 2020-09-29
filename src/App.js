434df364b40163be4ed6a2449b6228ed0972eec6
Antonio Felix <anfelix@csumb.edu>
2020-09-28 19:00:10 -0700

    small change

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/Login';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+    <Router>
+        <div>
+          <h2>Welcome to React Router Tutorial</h2>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <ul className="navbar-nav mr-auto">
+            <li><Link to = {'/'} class = "nav-link"> Home </Link></li>
+            <li><Link to={'/Login'} className="nav-link"> Login </Link></li>
+          </ul>
+          </nav>
+          <hr />
+          <Switch>
+            <Route exact path = '/' component={Home} />
+            <Route exact path='/Login' component={Login} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/Routes.js
@@ -0,0 +1,7 @@
+import { Route } from 'react-router';
+import './containers/Login';
+import Login from './containers/Login';
+
+<Route exact path = "/login">
+    <Login></Login>
+</Route>
