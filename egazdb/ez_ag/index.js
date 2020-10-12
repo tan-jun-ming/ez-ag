d513e59c50d403c41baa0e9005cca92cbec5cda2
Antonio Felix <anfelix@csumb.edu>
2020-10-11 17:22:33 -0700

    more database work

--- egazdb/ez_ag/index.js
+++ egazdb/ez_ag/index.js
@@ -1,40 +1,67 @@
 const express = require("express");
 const app = express();
-const cors = require("cors");
-const PORT = 4000;
+const MongoClient = require('mongodb').MongoClient
+const bodyParser= require('body-parser');
+const { db } = require("./user");
+var ObjectID = require('mongodb').ObjectID;
 
-const { UserSeed } = require('./user_seed.js');
+app.use(bodyParser.json());
+app.unsubscribe(bodyParser.urlencoded({extended: true}));
 
-const mongoose = require("mongoose");
+MongoClient.connect('mongodb+srv://anfelix:oaHKXziPb8dZcua3@ezagdb.qf03a.mongodb.net/anfelix?retryWrites=true&w=majority', (err, db) => {
+    if (err) return console.log(err)
 
-let user = require("./user");
+    app.listen(3000, () =>{
+        console.log('app working on 3000')
+    });
 
-app.use(cors());
+    let dbase = db.db('userdb')
 
-mongoose.connect("mongodb://127.0.0.1:27017/users", {
-    useNewUrlParser: true
-});
+    app.post('user/add', (req, res, next) => {
+        var user = {
+            email: req.body.email,
+            name: req.body.first_name + " " + req.body.last_name,
+            isAdmin: req.body.isAdmin
+        };
+    
+        dbase.collection('user').insertOne(user, (err, result) => {
+            if(err) 
+                console.log(err);
+            
+            res.send(result + " has been added");
+        });
+    });
+    
+    app.get('/user', (req, res, next) => {
+        dbase.collection('user').find().toArray((err, results) =>{
+            console.log(results)
+            res.send(results)
+        });
+    });
 
-const router = express.Router();
-app.use("/", router);
-
-router.route("/getUser").get(function(req, res){
-    user.find({}, function(err, result){
-        console.log(result);
-        if(err)
-            res.send(err);
-        else
+    app.get('/user/:id', (req, res, next) =>{
+       if(err)
+        throw err;
+        
+        let id = ObjectID(req.params.id);
+        db.collection('user').find(id).toArray((err, result) =>{
+            if(err)
+                throw err;
+            
             res.send(result);
+        })
     });
 });
 
-const connection = mongoose.connection;
+// const mongoClient = require('mongodb').MongoClient
+// var ObjectId = require('mongodb').ObjectId;
 
-
-connection.once("open", function(){
-    console.log("Connection with MongoDB was successful");
-});
-
-app.listen(PORT, function(){
-    console.log("Server is running on Port: " + PORT);
-});
+// MongoClient.connect(
+//     'mongodb+srv://dbusers:Cwth149t65ZhMKOY@ezagdb.qf03a.mongodb.net/dbusers?retryWrites=true&w=majority',
+//     (err, database) =>{
+//         var dbase = db.db("EzAgDb");
+//         if(err) return console.log(err)
+//         app.listen(3000, () => {
+//             console.long('app working on 300')
+//         })
+//     });
