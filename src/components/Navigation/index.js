318d416b708ea10c1813f44ba69edc1bc46face0
Antonio Felix <anfelix@csumb.edu>
2020-11-15 16:17:38 -0800

    sytax error :/

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -21,6 +21,7 @@ const NavigationAuth = () => (
     <li><Link to={ROUTES.LANDING}> Ez-Ag </Link></li>
     <li><Link to={ROUTES.HOME}>Home</Link></li>
     <li><Link to={ROUTES.ACCOUNT}>Account</Link> </li>
+    <li><Link to={ROUTES.TABLE}>Table</Link></li>
     <li><Link to={ROUTES.ADMIN}>Admin</Link></li>
     <li style = {{float:'right'}}><SignOutButton/></li>
     </ul>
@@ -32,26 +33,8 @@ const NavigationNonAuth = () => (
     <li><Link to={ROUTES.LANDING}> Ez-Ag </Link></li>
     <li style = {{float:'right'}}><Link to={ROUTES.SIGN_IN}> Sign In</Link></li>
     <li style = {{float:'right'}}><Link to={ROUTES.SIGN_UP}> Sign Up </Link></li>
+
   </ul>
-        <li>
-          <Link to={ROUTES.LANDING}>Landing</Link>
-        </li>
-        <li>
-            <Link to={ROUTES.HOME}>Home</Link>
-        </li>
-        <li>
-            <Link to={ROUTES.ACCOUNT}>Account</Link>
-        </li>
-        <li>
-            <Link to={ROUTES.ADMIN}>Admin</Link>
-        </li>
-        <li>
-            <Link to={ROUTES.TABLE}>Table</Link>
-        </li>
-        <li>
-            <SignOutButton />
-        </li>
-    </ul>
 );
 
 export default Navigation;
